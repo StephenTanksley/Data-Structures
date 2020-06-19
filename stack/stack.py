f8d05807370b222ac55215a224bd3f4154acef31
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-18 20:26:21 -0500

    Almost finished.

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -11,34 +13,61 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
+# Implemented using a list.
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.size += 1
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) == 0:
+#             print("There's nothing to remove.")
+#             return None
+#         else:
+#             removed = self.storage.pop(self.size - 1)
+#             self.size -= 1
+#             return removed
+
+
+# Implemented using a Linked List
 
 class Stack:
+
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return len(self.storage)
 
     def push(self, value):
         self.size += 1
-        return self.storage.append(value)
+        self.storage.add_to_head(value)
 
     def pop(self):
-        if(self.size > 0):
-            removed = self.storage.pop(self.size - 1)
-            self.size -= 1
-            return removed
-        else:
+        if self.size == 0:
             print("There's nothing to remove.")
+            return None
+        else:
+            self.storage.remove_head()
+            self.size -= 1
 
 
 stack = Stack()
 stack.push(1)
 stack.push(2)
 stack.push(3)
-stack.push(4)
+stack.pop()
 
-print(stack.storage)
-print(stack.pop())
-print(stack.storage)
+# Should only have 2, 1 returning
+
+stack.storage.print_list()
