ba16fdc7c10a210780ed49741dd2022f54571cc4
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-25 20:06:32 -0500

    Typed out code. Now to debug.

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,9 +1,60 @@
+
+"""THIS IS THE QUEUE CLASS """
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        added = self.storage.insert(len(self.storage), value)
+        self.size += 1
+        return added
+
+    def dequeue(self):
+        if(self.size > 0):
+            removed = self.storage.pop(0)
+            self.size -= 1
+            return removed
+        else:
+            print("There's nothing to remove.")
+
+
+""" THIS IS THE STACK CLASS """
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+        return len(self.storage)
+
+    def push(self, value):
+        self.size += 1
+        return self.storage.append(value)
+
+    def pop(self):
+        if len(self.storage) == 0:
+            print("There's nothing to remove.")
+            return None
+        else:
+            removed = self.storage.pop(self.size - 1)
+            self.size -= 1
+            return removed
+
+
 """
-Binartrailing_node search trees are a data structure that enforce an ordering over 
-the data thetrailing_node store. That ordering in turn makes it a lot more efficient 
+Binary search trees are a data structure that enforce an ordering over 
+the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
 
-This part of the project comprises two datrailing_nodes:
+This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
    on the BSTNode class.
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
@@ -11,11 +62,11 @@ This part of the project comprises two datrailing_nodes:
 """
 
 """
-A binartrailing_node search tree has a few rules.
+A binary search tree has a few rules.
 
 1) Must be a node-based construct.
-2) The left subtree of a node contains onltrailing_node nodes with ketrailing_nodes lesser than the original node's ketrailing_node.
-3) the right subtree of a node contains onltrailing_node nodes with ketrailing_nodes greater than the original node's ketrailing_node.
+2) The left subtree of a node contains only nodes with keys lesser than the original node's key.
+3) the right subtree of a node contains only nodes with keys greater than the original node's key.
 4) The left and right subtrees of the original node
 
 """
@@ -97,32 +148,66 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
-    # Print the value of evertrailing_node node, starting with the given node,
-    # in an iterative breadth first traversal
+
+        if node:
+            if node.left:
+                node.in_order_print(node.left)
+            print(node.value)
+            if node.right:
+                node.in_order_print(node.right)
+        # Print the value of every node, starting with the given node,
+        # in an iterative breadth first traversal
 
     def bft_print(self, node):
-        # Create a queue for nodes.
-        # Add the first node to the queue.
-        # While the queue is not empty
-        # remove the first node from the queue and store it in a current_node variable.
-        # print the removed node.
-        # Add all the children into the queue.
 
-        # Print the value of every  node, starting with the given node,
-        # in an iterative depth first traversal
+        # 1) - Create a queue for nodes.
+        queue = Queue()
 
-        pass
+        # 1.5) - Check for null values.
+        if node.value:
+
+            # 2) - Add the first node to the queue.
+            queue.enqueue(node)
+            print(queue.storage)
+
+            # 3) - While the queue is not empty
+            while queue is not None:
+
+                # 4) - remove the first node from the queue and store it in a current_node variable.
+                current_node = queue.dequeue()
+
+                # 5) - print the removed node.
+                print(current_node.value)
+
+                # 6) - Add all the children into the queue.
+                queue.enqueue(current_node.left)
+                queue.enqueue(current_node.right)
+            else:
+                return
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
 
     def dft_print(self, node):
 
-        # Create a stack for nodes.
-        # Add the first node to the stack.
-        # While the stack is not empty
-        # get the current node from the top of the stack.
-        # print that node.
-        # add all children to the stack. The order you add the children will matter.
-        pass
+        # 1) - Create a stack for nodes.
+        stack = Stack()
+
+        # 2) - Add the first node to the stack.
+        stack.push(node)
+
+        # 3) - While the stack is not empty
+        while stack is not None:
+
+            # 4) - get the current node from the top of the stack.
+            current_node = stack.pop()
+
+            # 5) - print that node.
+            print(current_node.value)
+
+            # 6) - add all children to the stack. The order you add the children will matter.
+            stack.push(current_node.right)
+            stack.push(current_node.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
